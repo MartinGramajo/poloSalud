2a479fdd265d2189ce8e2b39fc193184cf65b57b
¨mago¨ <martingramajo08@gmail.com>
2024-04-18 21:26:20 -0300

    PROBANDO

--- src/components/common/NavReact.jsx
+++ src/components/common/NavReact.jsx
@@ -1,6 +1,6 @@
 import { Container, Image, Nav, Navbar } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
-import logoNuevo from "../../assets/img/logoblanco.png";
+import logoNuevo from "../../assets/img/logoBlanco.png";
 import linken from "../../assets/img/linken.png";
 import insta from "../../assets/img/insta.png";
 
